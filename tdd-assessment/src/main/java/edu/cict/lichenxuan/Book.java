e574f1094505885fb556ba51f4e957240e509a0d
lcx <8589835+henan-animal-husbandry_0@user.noreply.gitee.com>
2025-10-17 18:32:10 +0100

    three entities

---
+++ tdd-assessment/src/main/java/edu/cict/lichenxuan/Book.java
@@ -0,0 +1,33 @@
+package edu.cict.lichenxuan;
+
+public class Book {
+    String id;
+
+    String title;
+
+    int copiesAvailable;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
+}
---
+++ tdd-assessment/src/main/java/edu/cict/lichenxuan/Reservation.java
@@ -0,0 +1,23 @@
+package edu.cict.lichenxuan;
+
+public class Reservation {
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    String userId;
+
+    String bookId;
+}
