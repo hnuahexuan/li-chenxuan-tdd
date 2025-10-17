2e7e88105f220a8757a7de459d8d98fa78e8dc3d
lcx <8589835+henan-animal-husbandry_0@user.noreply.gitee.com>
2025-10-17 18:36:26 +0100

    two interfaces

---
+++ tdd-assessment/src/main/java/edu/cict/lichenxuan/IBookRepository.java
@@ -0,0 +1,11 @@
+package edu.cict.lichenxuan;
+
+import java.util.*;
+interface IBookRepository {
+    Book findById(String id);
+
+    void save(Book book);
+
+    List<Book> findAll();
+}
+
---
+++ tdd-assessment/src/main/java/edu/cict/lichenxuan/IReservationRepository.java
@@ -0,0 +1,13 @@
+package edu.cict.lichenxuan;
+
+import java.util.*;
+interface IReservationRepository {
+    void save(Reservation reservation);
+
+    boolean existsByUserAndBook(String userId, String bookId);
+
+    List<Reservation> findByUser(String userId);
+
+    void delete(String userId, String bookId);
+}
+
